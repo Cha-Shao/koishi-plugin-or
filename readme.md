c02d9d25560e902d3cd50d5df86aed32cb5b8dac
Cha_Shao <yrchashao@outlook.com>
2023-07-06 22:10:37 +0800

    first commit

--- readme.md
+++ readme.md
@@ -3,3 +3,43 @@
 [![npm](https://img.shields.io/npm/v/@yrchashao/koishi-plugin-or?style=flat-square)](https://www.npmjs.com/package/@yrchashao/koishi-plugin-or)
 
 选择困难帮助器
+
+## 指令 choice
+
+返回提供选项的若干个
+
+用法: `choice [count] <...options>`
+
+### 例：
+
+从小红、小橙、小黄、小绿、小青中选择2个
+
+`choice 2 小红 小橙 小黄 小绿 小青`
+
+`> 小红，小青`
+
+## 指令 or
+
+从2个选项中选择1个
+
+用法 `or <optionA> <optionB>`
+
+### 例：
+
+`or 麦当劳 肯德基`
+
+`> 麦当劳`
+
+## 指令 allow
+
+可以或不可以
+
+用法：`allow <option>`
+
+### 例：
+
+可不可以瑟瑟
+
+`allow 瑟瑟`
+
+`> 不可以瑟瑟`
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,32 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'or'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+function getRandomOptions(count: number, options: any[]): any[] {
+  const result: any[] = [];
+  const length = options.length;
+  for (let i = 0; i < count; i++) {
+    const randomIndex = Math.floor(Math.random() * length);
+    result.push(options[randomIndex]);
+  }
+  return result;
+}
+
+function choice(optionA: any, optionB: any): any {
+  const randomIndex = Math.floor(Math.random() * 2);
+  return randomIndex === 0 ? optionA : optionB;
+}
+
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command("choice [count] <...options>")
+    .action((_, count, ...options) => {
+      getRandomOptions(parseInt(count), options)
+    })
+  ctx.command("or <optionA> <optionB>")
+    .action((_, optionA, optionB) => choice(optionA, optionB))
+  ctx.command("allow <option>")
+    .action((_, option) => choice("可以", "不可以") + option)
 }
